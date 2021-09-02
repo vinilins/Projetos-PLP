6d1453a8e71a90e8fac1a1e26eafbee2f57fbdc8
Lucas Araújo de Lima <70279700+lucasarlim@users.noreply.github.com>
2021-09-01 22:18:36 -0300

    Updated Ajuda

--- app/Main.hs
+++ app/Main.hs
@@ -13,27 +13,33 @@ ajuda :: IO()
 ajuda = do
     cls
     putStrLn "\n------------------------------------------------------------------------- Como Jogar ------------------------------------------------------------------------\n"
-    putStrLn "Para se iniciar a partida, joga-se o dado e o participante que fizer o maior \n\ 
-    \número de pontos (6) inicia o jogo, continuando as jogadas em sentido horário. \n\
-    \Joga-se com um dado e os avanços são feitos de acordo com os pontos obtidos com o lançamento \n\
-    \dos dados. Cada jogador lança o dado e se tirar (6) poderá sair da casa de partida, sendo que, ao \n\
-    \tirar o mesmo, você tem o direito de jogar o dado novamente.\n\n\ 
-    \Pode ser jogado por um player e um bot baseado em heurística e aleatoriedade. \n\
-    \O tabuleiro quadrado tem um percurso em forma de cruz e cada jogador tem quatro peões. \n\
-    \Um dado define os movimentos. Os peões de cada jogador começam na base de mesma cor. \n\
-    \O objetivo do jogo é ser o primeiro a levar seus 4 peões a dar uma volta no tabuleiro \n\
-    \e a chegar no ponto final marcado com sua cor. Os peões movem-se pelo percurso no sentido horário. \n\
-    \Para transportar um peão de sua base para seu ponto de partida é necessário tirar 6. \n\n\
-    \Quando o jogador já tem pelo menos um peão no percurso, ele pode mover o peão do número de \n\
-    \casas tirado no dado. Se tirar 6, além de usar esse resultado ele pode jogar novamente o dado. \n\
-    \Se um jogador chegar a uma casa já ocupada por um peão adversario, o peão adversário \n\
-    \deve voltar para sua base. Mas se 2 peões da mesma cor ocuparem uma mesma casa, eles não podem \n\
-    \ser capturados e nenhum adversário pode passar por essa casa, tendo seus peões bloqueados. \n\
-    \Após dar a volta no tabuleiro o peão avança pela reta final, de sua própria cor. \n\
-    \A chegada ao ponto final só pode ser obtida por um número exato nos dados. \n\
-    \Se o jogador tirar mais do que o necessário, ele vai ate o fim e volta, tendo que aguardar \n\
-    \sua próxima jogada.\n\n\ 
-    \O vencedor é o primeiro a levar seus quatro peões ao ponto de chegada da sua cor.\n"
+    putStrLn "🎯 Objetivo do jogo
+Percorrer todo o trajeto do tabuleiro no sentido horario com todas as peças e ser o primeiro jogador a levar seus quatro peões ao ponto de chegada da sua cor.
+
+
+📌 Regras
+Pode ser jogado por um player e um bot baseado em heurística e aleatoriedade.
+
+O tabuleiro quadrado tem um percurso em forma de cruz e cada jogador tem quatro peões.
+
+Os peões de cada jogador começam na base de mesma cor.
+
+Para se iniciar a partida, joga-se o dado e o participante que fizer o maior número de pontos (6) inicia o jogo retirando uma peça da base.
+
+Quando o jogador já tem pelo menos um peão no percurso, joga-se um dado e os avanços são feitos de acordo com os pontos obtidos com o lançamento do dado. Se tirar 6, além de usar esse resultado ele pode jogar novamente o dado.
+
+Para transportar um peão de sua base para seu ponto de partida é necessário tirar (6).
+
+Se um jogador chegar a uma casa já ocupada por um peão adversário, o peão adversário deve voltar para sua base.
+
+Mas se 2 peões da mesma cor ocuparem uma mesma casa, eles não podem ser capturados e nenhum adversário pode passar por essa casa, tendo seus peões bloqueados.
+
+Após dar a volta no tabuleiro o peão avança pela reta final, de sua própria cor. A chegada ao ponto final só pode ser obtida por um número exato nos dados. Se o jogador tirar mais do que o necessário, ele vai até o fim e volta, tendo que aguardar sua próxima jogada.
+
+✅ Outras Funcionalidades:
+O jogo terá um sistema de salvamento;
+Terá obstáculos pelo caminho como uma casa que retrocede duas casas;
+\n"
     putStrLn $ setColorGreen "Pressione <Enter> para voltar\n"
     getChar -- descarta o enter
     main
